79bcae4fe09db8b1864d91bdbab4c51b4ac404da
unknown <kkshtj40@gmail.com>
2018-12-13 14:59:25 +0530

    initial commit

---
+++ components/scripts.php
@@ -0,0 +1,6 @@
+<script src="assets/scripts/jquery-3.3.1.min.js"></script>
+<script src="assets/scripts/popper.min.js"></script>
+<script src="assets/scripts/bootstrap.min.js"></script>
+<script src="assets/scripts/fontawesome-all.min.js"></script>
+
+<script src="assets/scripts/main.js"></script>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<?php include('components/links.php'); ?>
+
+		<title>Home | IIT (ISM), Dhanbad</title>
+
+	</head>
+	<body>
+
+		<?php include('components/header.php'); ?>
+
+		<div class="body">
+		</div>
+
+		<?php include('components/footer.php'); ?>
+
+		<?php include('components/scripts.php'); ?>
+
+	</body>
+</html>
